63c5d96edf6f60865c43709426ed0d75dd9b71a7
Dylan Kendal <dylankendal@gmail.com>
2013-08-04 03:02:17 -0700

    name search works

---
+++ PartSearchGui.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace part_search {
+  [KSPAddon( KSPAddon.Startup.EditorAny, true )]
+  public class PartSearchGui : MonoBehaviour {
+#region properties
+    int id;
+    Rect window_position = new Rect();
+    List<SearchFilter> search_filters = new List<SearchFilter>();
+
+    string _search_text = "";
+    string SearchText {
+      get { return _search_text; }
+      set {
+        var old_val = SearchText;
+        _search_text = value;
+        if( old_val.Equals( value ) == false ) {
+          OnSearchTextChanged();
+        }
+      }
+    }
+#endregion
+
+#region unity_event_handlers
+    void Awake() {
+      id = GUIUtility.GetControlID( FocusType.Passive );
+      AddSearchFilters();
+    }
+
+    void OnGUI() {
+      window_position = GUILayout.Window( id, window_position, DrawPartSearchWindow, "" );
+    }
+#endregion
+
+    void OnSearchTextChanged() {
+      EditorPartList.Instance.Refresh();
+    }
+
+    void DrawPartSearchWindow(int window_id) {
+      SearchText = GUILayout.TextField( SearchText, GUILayout.MinWidth( 300f ) );
+      GUI.DragWindow();
+    }
+
+    void AddSearchFilters() {
+      var title_search = new SearchFilter(
+        (AvailablePart p) => Regex.IsMatch( p.title, "(?i)" + SearchText ) );
+
+      search_filters.Add( title_search );
+
+      search_filters.ForEach( x => x.Enabled = true );
+    }
+  }
+}
---
+++ SearchFilter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+namespace part_search {
+  class SearchFilter : EditorPartListFilter{
+
+    bool _enabled = false;
+    public bool Enabled {
+      set {
+        if( Enabled == value ) { return; }
+
+        _enabled = value;
+        switch( Enabled) {
+          case true:
+            EditorPartList.Instance.ExcludeFilters.AddFilter( this );
+            break;
+          case false:
+            EditorPartList.Instance.ExcludeFilters.RemoveFilter( this );
+            break;
+        }
+      }
+      get { return _enabled; }
+    }
+
+    public SearchFilter(Func<AvailablePart, bool> criteria) :
+      base( new Guid().ToString(), criteria ){
+    }
+  }
+}
