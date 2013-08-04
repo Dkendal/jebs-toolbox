21f6e6e58e0873b07ff6dcd57bdf5b33afe71a16
Dylan Kendal <dylankendal@gmail.com>
2013-08-03 18:46:02 -0700

    renders a textbox on vab scene

---
+++ PartSearchTool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+[KSPAddon( KSPAddon.Startup.EditorAny, true )]
+public class PartSearchTool : MonoBehaviour {
+
+  private int id;
+  private Rect window_position = new Rect();
+  private string search_text;
+  
+  void Awake() {
+    id = GUIUtility.GetControlID(FocusType.Passive);
+    search_text = "search for a part";
+    window_position.x = 200;
+  }
+
+  void Update(){
+  }
+
+  void OnGUI() {
+    window_position = GUILayout.Window( id, window_position, DrawPartSearchWindow, "" );
+  }
+
+  void DrawPartSearchWindow(int window_id) {
+    search_text = GUILayout.TextField( search_text, GUILayout.MinWidth(300f));
+    GUI.DragWindow();
+  }
+} 
