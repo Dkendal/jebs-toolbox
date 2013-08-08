4136193b3f2a80693014e2196dfef8bd9ea72992
Dylan C. Kendal <dkendal@freerunningtechnologies.com>
2013-08-07 22:45:24 -0700

    Update AssemblyInfo.cs
    Changed assembly name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("part_search")]
+[assembly: AssemblyTitle("Jebs-Toolbox")]
 [assembly: AssemblyDescription( "A part finder utility for ksp" )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany( "github.com/dkendal" )]
-[assembly: AssemblyProduct("part_search")]
+[assembly: AssemblyProduct("Jebs-Toolbox")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
